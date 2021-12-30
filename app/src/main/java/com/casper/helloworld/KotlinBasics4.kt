acce6cf555ce339b6310bcf9ae6bdf7990f431fb
uwais <n01378141@humbermail.ca>
2021-12-30 11:52:52 -0500

    added interface and abstract class examples.

--- app/src/main/java/com/casper/helloworld/KotlinBasics4.kt
+++ app/src/main/java/com/casper/helloworld/KotlinBasics4.kt
@@ -1,5 +1,23 @@
 package com.casper.helloworld
 
+// Inheritance is one of the main concepts of
+// Object Oriented Programming (OOP).
+///It allows a class to inherit features
+//(properties and methods) from another class and reuse them.
+// The class that inherits the features of another
+// class is called the Sub class or Child class or
+// Derived class , and the class whose features are
+// inherited is called the Super class or Parent class
+// or Base class.
+// All the classes in Kotlin inherit from a class called Any. It corresponds to the Object class in Java.
+// Every class that you create in Kotlin implicitly
+// inherits from Any -
+
+
+// notice the keyword open. All classes in
+// Kotlin are final by default (non-inheritable),
+// so you need to use the open Keyword to make a
+// class inheritable
 open class Cars(val name: String, val brand: String) {
     // open so it can be overriden by inhereting classes
     open var range: Double = 0.0
